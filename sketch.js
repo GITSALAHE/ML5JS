a18da08abcf3580be63e41fc9f3cceae4389096e
GITSALAHE <salah.bouanba2@gmail.com>
2020-08-04 22:34:26 +0100

    done ML5js

---
+++ sketch.js
@@ -0,0 +1,57 @@
+// Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
+// Create a YOLO method
+const yolo = ml5.YOLO(modelReady);
+let img;
+let objects = [];
+let status;
+
+function setup() {
+  createCanvas(640, 420);
+  img = createImg('dog.jpg', imageReady);
+  img.hide();
+  img.size(640, 420);
+
+}
+
+// Change the status when the model loads.
+function modelReady() {
+  console.log("model Ready!")
+  status = true;
+}
+
+// When the image has been loaded,
+// get a prediction for that image
+function imageReady() {
+  console.log('Detecting') 
+  yolo.detect(img, gotResult);
+}
+
+// A function to run when we get any errors and the results
+function gotResult(err, results) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(results)
+  
+  objects = results;
+  console.log(objects)
+  document.getElementById('res').innerHTML = 'Model detected ' + objects[0].label;
+  document.getElementById('conf').innerHTML = 'Confidence ' + objects[0].confidence;
+}
+
+
+function draw() {
+  if (status != undefined) {
+    image(img, 0, 0)
+
+    for (let i = 0; i < objects.length; i++) {
+      noStroke();
+      fill(0, 255, 0);
+      text(objects[i].label + " " + nfc(objects[i].confidence * 100.0, 2) + "%", objects[i].x * width + 5, objects[i].y * height + 15);
+      noFill();
+      strokeWeight(4);
+      stroke(0, 255, 0);
+      rect(objects[i].x * width, objects[i].y * height, objects[i].w * width, objects[i].h * height);
+    }
+  }
+}
